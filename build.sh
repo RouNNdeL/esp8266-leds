bf79c54bb8cb28d7b9fa8a6df59527d4fa7f41a4
RouNdeL <krzys.zdulski@gmail.com>
2018-05-09 12:52:30 +0200

    Add a build script to build and copy binaries to device folders

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git pull
+platformio run
+VERSION_CODE=$(cat src/config.h | sed -nr 's/#define VERSION_CODE ([0-9]+)/\1/p')
+cd .pioenvs/
+for d in */; do
+mkdir -p $1/$d
+cp $d/firmware.bin $1/$d/bin_$VERSION_CODE.bin
+done
