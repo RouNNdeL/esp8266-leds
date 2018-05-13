4891525ebb059e4e8df395c6d95d5ff04cbd01bb
RouNdeL <krzys.zdulski@gmail.com>
2018-05-13 16:05:02 +0200

    Fix deploy key permissions and change the decryption path to /tmp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ cd "$parent_path"
 VERSION_CODE=$(cat ../src/config.h | sed -nr 's/#define VERSION_CODE ([0-9]+)/\1/p')
 cd ./../.pioenvs/
 for d in */; do
-ssh -i ../deploy_key -o "UserKnownHostsFile ../known_hosts" pi@zdul.xyz "mkdir -p /var/www/html/smart/iot_binaries/$d"
-scp -i ../deploy_key -o "UserKnownHostsFile ../known_hosts" $d\firmware.bin pi@zdul.xyz:/var/www/html/smart/iot_binaries/$d/bin_$VERSION_CODE.bin
+ssh -i /tmp/deploy_key -o "UserKnownHostsFile ../known_hosts" pi@zdul.xyz "mkdir -p /var/www/html/smart/iot_binaries/$d"
+scp -i /tmp/deploy_key -o "UserKnownHostsFile ../known_hosts" $d\firmware.bin pi@zdul.xyz:/var/www/html/smart/iot_binaries/$d/bin_$VERSION_CODE.bin
 done
