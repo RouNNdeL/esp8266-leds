cec624010c4c98eeb53846dc36da096db5cd3c40
RouNdeL <krzys.zdulski@gmail.com>
2018-05-13 17:11:25 +0200

    Change user to travis_build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ cd "$parent_path"
 VERSION_CODE=$(cat ../src/config.h | sed -nr 's/#define VERSION_CODE ([0-9]+)/\1/p')
 cd ./../.pioenvs/
 for d in */; do
-ssh -i /tmp/deploy_key -o "UserKnownHostsFile ../known_hosts" pi@zdul.xyz "mkdir -p /var/www/html/smart/iot_binaries/$d"
-scp -i /tmp/deploy_key -o "UserKnownHostsFile ../known_hosts" $d\firmware.bin pi@zdul.xyz:/var/www/html/smart/iot_binaries/$d/bin_$VERSION_CODE.bin
+ssh -i /tmp/deploy_key -o "UserKnownHostsFile ../known_hosts" travis_build@zdul.xyz "mkdir -p /var/www/html/smart/iot_binaries/$d"
+scp -i /tmp/deploy_key -o "UserKnownHostsFile ../known_hosts" $d\firmware.bin travis_build@zdul.xyz:/var/www/html/smart/iot_binaries/$d/bin_$VERSION_CODE.bin
 done
