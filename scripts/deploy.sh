d5d332e8181a71cad420245f7447ea17f93a82b7
RouNdeL <krzys.zdulski@gmail.com>
2018-05-13 15:51:13 +0200

    Add a known_hosts file and use it in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
 
-VERSION_CODE=$(cat src/config.h | sed -nr 's/#define VERSION_CODE ([0-9]+)/\1/p')
+VERSION_CODE=$(cat ../src/config.h | sed -nr 's/#define VERSION_CODE ([0-9]+)/\1/p')
 cd ./../.pioenvs/
 for d in */; do
-mkdir -p $1/$d
-scp -i deploy_key $d\firmware.bin pi@zdul.xyz:/var/www/html/smart/iot_binaries/$d/bin_$VERSION_CODE.bin
+ssh -i ../deploy_key -o "UserKnownHostsFile ../known_hosts" pi@zdul.xyz "mkdir -p /var/www/html/smart/iot_binaries/$d"
+scp -i ../deploy_key -o "UserKnownHostsFile ../known_hosts" $d\firmware.bin pi@zdul.xyz:/var/www/html/smart/iot_binaries/$d/bin_$VERSION_CODE.bin
 done
